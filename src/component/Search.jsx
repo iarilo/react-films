160eb28c5bb2c440f6ab03be2e62e3115dedd823
Vitaly <brynzav2@gmail.com>
2022-02-14 19:11:28 +0200

    Переделал с Классов на функционал(хуки )

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,32 +1,128 @@
-    import React from 'react';
+    import React,{useState} from 'react';
 
-export class FanSearch extends React.Component {
+ //export function FanSearch({ searcMovies }) {
+ export function FanSearch (props) { 
+     const {
+         searchFilm = Function.prototype,
+        } = props;
+    const [categories, setCategories] = useState('');
+    const [Type,setType] = useState('all');
+  /*   constructor() {
+        super();
+        this.state = {
+            categories: '',
+            Type: 'all',
+        };
+        this.valueSearch = this.valueSearch.bind(this);
+        this.handleKey = this.handleKey.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+    } */
+
+      function valueSearch(event) {
+        setCategories(event.target.value);
+    }
+     function handleKey(event) {
+        if (event.key === 'Enter') {
+            searchFilm(categories, Type);
+        }
+    }
+     function handleFilter(event) {
+        //dataset коллекция атрибутов
+         setType(event.target.dataset.type);
+         searchFilm(categories,event.target.dataset.type);
+}
+
+   
+        return (
+            <div className="row">
+                <div className="col s12">
+                    <div className="input-field inline">
+                        <input
+                            id="email_inline"
+                            className="validate"
+                            placeholder="search"
+                            type="email"
+                            value={categories}
+                            onChange={valueSearch}
+                            onKeyDown={handleKey}
+                        />
+                        <button className="btn search-btn"
+                            onClick={() => searchFilm(
+                                categories,Type)} >
+                            поиск
+                        </button>
+
+
+                        <form action="#" className="radio">
+                            <p>
+                                <label>
+                                    <input className="with-gap"
+                                        name="Type" type="radio" data-type=" all"
+                                        onChange={handleFilter}
+                                        checked={Type === 'all'} />
+                                    <span>All</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    <input className="with-gap"
+                                        name="Type" type="radio" data-type="movie"
+                                        onChange={handleFilter}
+                                        checked={Type === 'movie'} />
+                                    <span>movie only</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    <input className="with-gap"
+                                        name="Type" type="radio" data-type="series"
+                                        onChange={handleFilter}
+                                        checked={Type === 'series'} />
+                                    <span>Series only</span>
+                                </label>
+                            </p>
+
+                            <p>
+                                <label>
+                                    <input className="with-gap"
+                                        name="Type" type="radio" data-type="game"
+                                        onChange={handleFilter}
+                                        checked={Type === 'game'} />
+                                    <span>Game</span>
+                                </label>
+                            </p>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+ // КЛАССЫ =================================================
+
+/* export class FanSearch extends React.Component {
     constructor() {
         super();
         this.state = {
             categories: '',
             Type: 'all',
-
         };
         this.valueSearch = this.valueSearch.bind(this);
         this.handleKey = this.handleKey.bind(this);
         this.handleFilter = this.handleFilter.bind(this);
-        
-    }
+   }
 
     valueSearch(event) {
         this.setState({ categories: event.target.value });
-       
     }
-
-    handleKey(event) {
+   handleKey(event) {
         if (event.key === 'Enter') {
         this.props.searcMovies(
         this.state.categories, this.state.Type);
-            
         }
     }
-
     handleFilter(event) {
         //dataset коллекция атрибутов
         this.setState(
@@ -57,8 +153,8 @@ export class FanSearch extends React.Component {
                    поиск
  </button>
 
-{/* <form action="#" className="radio"> */}
-<div action="#" className="radio">
+
+<form action="#" className="radio">
   <p>
     <label>
         <input className="with-gap"
@@ -98,11 +194,11 @@ export class FanSearch extends React.Component {
     </p>
 
 
-</div>
+</form>
                     </div>
                 </div>
             </div>
         );
     }
-}
+} */
  
--- src/loiaut/File_Main.jsx
+++ src/loiaut/File_Main.jsx
@@ -1,11 +1,63 @@
- import React from 'react';
+ import React,{useState,useEffect} from 'react';
 import { FunListmain } from '../component/ListMain';
 import { FanPreloader } from '../component/Preloader';
 import { FanSearch } from '../component/Search';
 
- const API_KEY = process.env.REACT_APP_API_KEY;
 
-class FanMain extends React.Component {
+function FanMain (){
+  const [films,setFilms] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+
+  function searchMovies(str, type = 'all') {
+    setLoading(true);
+    //&type=${type} & скрепление гет параметров
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFilms( data.Search);
+        setLoading(false);
+        /*  .catch((err) => {
+           console.error(err);
+            setLoading(false);;
+            });
+         */
+      });
+  }
+
+ useEffect(()=>{
+   fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
+     .then(response => response.json())
+     .then(data => {
+        setFilms(data.Search);
+         setLoading(false);});
+       /*  .catch((err) => {
+            console.error(err);
+            setLoading(false);;
+           });
+        */
+  
+ },[]);
+
+
+   return (
+      <main className='container content'>
+        <FanSearch
+          searchFilm={searchMovies}
+        />
+        {loading ? <FanPreloader />
+          : <FunListmain AppMovies={films} />
+          /*AppList={this.state.film.Search}*/
+        }
+      </main>
+    );
+  
+};
+export default FanMain; 
+
+ //CLASS ===================================
+ // API_KEY = process.env.REACT_APP_API_KEY;
+/*   class FanMain extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -15,9 +67,8 @@ class FanMain extends React.Component {
     };
     this.searchMovies = this.searchMovies.bind(this)
   }
-
   componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
       .then(response => response.json())
       .then(data => this.setState(
         {films:data.Search,loading:false }))
@@ -25,11 +76,10 @@ class FanMain extends React.Component {
          this.setState({loading:false});
         });
   }
-
   searchMovies(str, type = 'all') {
     this.setState({loading:true});
     //&type=${type} & скрепление гет параметров
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then((response) => response.json())
       .then((data) => {
        this.setState({films:data.Search, loading:false})
@@ -48,10 +98,10 @@ class FanMain extends React.Component {
         />
         {loading ? <FanPreloader />
        : <FunListmain AppMovies={films} />
-          /*AppList={this.state.film.Search}*/  
+          /*AppList={this.state.film.Search}  
         }
       </main>
     );
   }
 };
-export default FanMain; 
+export default FanMain;  */
