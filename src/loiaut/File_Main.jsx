071c4c9d8ca1bd0ae1ff59ec93f6551bf6dca05a
Vitaly <brynzav2@gmail.com>
2022-02-17 12:47:48 +0200

    Убрал пробел в json script

--- src/loiaut/File_Main.jsx
+++ src/loiaut/File_Main.jsx
@@ -3,6 +3,7 @@ import { FunListmain } from '../component/ListMain';
 import { FanPreloader } from '../component/Preloader';
 import { FanSearch } from '../component/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function FanMain (){
   const [films,setFilms] = useState([]);
@@ -12,31 +13,17 @@ function FanMain (){
   function searchMovies(str, type = 'all') {
     setLoading(true);
     //&type=${type} & скрепление гет параметров
-    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then((response) => response.json())
-      .then((data) => {
-        setFilms( data.Search);
-        setLoading(false);
-        /*  .catch((err) => {
-           console.error(err);
-            setLoading(false);;
-            });
-         */
-      });
-  }
+      .then((data)=>{setFilms( data.Search);setLoading(false);})
+   // .catch((err)=>{console.error(err); setLoading(false); });
+   }
 
  useEffect(()=>{
-   fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
+   fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
      .then(response => response.json())
-     .then(data => {
-        setFilms(data.Search);
-         setLoading(false);});
-       /*  .catch((err) => {
-            console.error(err);
-            setLoading(false);;
-           });
-        */
-  
+     .then(data => {setFilms(data.Search);setLoading(false);})
+  //.catch((err) => {console.error(err);setLoading(false); });
  },[]);
 
 
@@ -55,53 +42,4 @@ function FanMain (){
 };
 export default FanMain; 
 
- //CLASS ===================================
- // API_KEY = process.env.REACT_APP_API_KEY;
-/*   class FanMain extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      //film:{Search:[]}
-      films:[],
-       loading: true
-    };
-    this.searchMovies = this.searchMovies.bind(this)
-  }
-  componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
-      .then(response => response.json())
-      .then(data => this.setState(
-        {films:data.Search,loading:false }))
-        .catch( (err)=>{console.error(err);
-         this.setState({loading:false});
-        });
-  }
-  searchMovies(str, type = 'all') {
-    this.setState({loading:true});
-    //&type=${type} & скрепление гет параметров
-    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
-      .then((response) => response.json())
-      .then((data) => {
-       this.setState({films:data.Search, loading:false})
-       .catch((err) => {
-        console.error(err);
-        this.setState({ loading: false });
-    });
-  });
-} 
-  render() {
-    const {films,loading} = this.state;
-      return (
-      <main className='container content'>
-        <FanSearch 
-        searchFilm = {this.searchMovies}
-        />
-        {loading ? <FanPreloader />
-       : <FunListmain AppMovies={films} />
-          /*AppList={this.state.film.Search}  
-        }
-      </main>
-    );
-  }
-};
-export default FanMain;  */
+ 
