bb9ad0595f64b77c97b2f762d808caf535a7da4a
Vitaly <brynzav2@gmail.com>
2022-02-17 13:11:48 +0200

    добавил цифровой ключь

--- src/loiaut/File_Main.jsx
+++ src/loiaut/File_Main.jsx
@@ -13,14 +13,14 @@ function FanMain (){
   function searchMovies(str, type = 'all') {
     setLoading(true);
     //&type=${type} & скрепление гет параметров
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
       .then((response) => response.json())
       .then((data)=>{setFilms( data.Search);setLoading(false);})
    // .catch((err)=>{console.error(err); setLoading(false); });
    }
 
  useEffect(()=>{
-   fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+   fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
      .then(response => response.json())
      .then(data => {setFilms(data.Search);setLoading(false);})
   //.catch((err) => {console.error(err);setLoading(false); });
@@ -42,4 +42,55 @@ function FanMain (){
 };
 export default FanMain; 
 
+ //-------------------------------------------------
  
+//CLASS ===================================
+ // API_KEY = process.env.REACT_APP_API_KEY;
+/*   class FanMain extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      //film:{Search:[]}
+      films:[],
+       loading: true
+    };
+    this.searchMovies = this.searchMovies.bind(this)
+  }
+  componentDidMount() {
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=matrix`)
+      .then(response => response.json())
+      .then(data => this.setState(
+        {films:data.Search,loading:false }))
+        .catch( (err)=>{console.error(err);
+         this.setState({loading:false});
+        });
+  }
+  searchMovies(str, type = 'all') {
+    this.setState({loading:true});
+    //&type=${type} & скрепление гет параметров
+    fetch(`http://www.omdbapi.com/?apikey=27a5ebd5&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+      .then((response) => response.json())
+      .then((data) => {
+       this.setState({films:data.Search, loading:false})
+       .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+    });
+  });
+} 
+  render() {
+    const {films,loading} = this.state;
+      return (
+      <main className='container content'>
+        <FanSearch 
+        searchFilm = {this.searchMovies}
+        />
+        {loading ? <FanPreloader />
+       : <FunListmain AppMovies={films} />
+          /*AppList={this.state.film.Search}  
+        }
+      </main>
+    );
+  }
+};
+export default FanMain;  */
